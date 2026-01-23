c10e0d0d2bfacc9a8d7ebd4bfbd37ddc75baa743
Jan Berges <jan.berges@uni-bremen.de>
2026-01-23 12:20:24 +0100

    Use Rydberg rather than eV units

--- elphy.c
+++ elphy.c
@@ -37,7 +37,8 @@ double free_energy(const int ne, const double n,
     const double *e, const double kt, const double mu);
 
 int main(int argc, char **argv) {
-    const double kt = 0.025;
+    const double ry = 13.605693122990; /* 2022 CODATA */
+    const double kt = 0.025 / ry;
     double **h, *e, nel = 0.25, mu = 0.0;
     struct model m;
     int n, nc;
--- test.py
+++ test.py
@@ -6,16 +6,17 @@ import subprocess
 
 elphmod.misc.verbosity = 0
 
+kT = 0.025 / elphmod.misc.Ry
 nc = 12
 n = nc * nc
 
-el, ph, elph, elel = elphmod.models.graphene.create()
+el, ph, elph, elel = elphmod.models.graphene.create(rydberg=True)
 
 e = np.linalg.eigvalsh(el.supercell(nc, nc).H())
-mu = elphmod.occupations.find_Fermi_level(n, e)
+mu = elphmod.occupations.find_Fermi_level(n, e, kT)
 
 res = float(subprocess.check_output(['./elphy', str(nc)]))
 
-ref = elphmod.diagrams.grand_potential(e - mu) + mu * n
+ref = elphmod.diagrams.grand_potential(e - mu, kT) + mu * n
 
 assert np.allclose(res, ref)
