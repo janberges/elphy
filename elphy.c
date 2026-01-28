8390398c89095412c4ec3469c431973c54116161
Jan Berges <jan.berges@uni-bremen.de>
2026-01-28 11:32:27 +0100

    Choose consistent variable names

--- elphy.c
+++ elphy.c
@@ -1,9 +1,9 @@
 #include "elphy.h"
 
 int main(int argc, char **argv) {
-    double **h, **c, *e, *u, energy, *forces, *occ, nel, mu = 0.0;
+    double **h, **c, *e, *u, energy, *forces, *occ, n, mu = 0.0;
     struct model m;
-    int nc, n, nx, i, j, **cr, **cells;
+    int nc, nel, nph, i, j, **cr, **cells;
     char (*typ)[3];
     double (*tau)[3], uc[3][3];
 
@@ -16,47 +16,47 @@ int main(int argc, char **argv) {
 
     repeat(m, nc, cells, uc, typ, tau);
 
-    nx = m.nph * nc;
-    n = m.nel * nc;
-    nel = 0.5 * m.n * nc;
+    nel = m.nel * nc;
+    nph = m.nph * nc;
+    n = 0.5 * m.n * nc;
 
-    h = matrix(n);
-    c = matrix(nx);
+    h = matrix(nel);
+    c = matrix(nph);
 
     supercell(h, m.nel, m.nt, m.t, nc, cr);
     supercell(c, m.nph, m.nk, m.k, nc, cr);
 
-    u = malloc(nx * sizeof *u);
+    u = malloc(nph * sizeof *u);
     get_displ("u.dat", nc * m.nat, uc, typ, tau, u);
 
     /* put_displ("u_copy.dat", nc * m.nat, uc, typ, tau, u); */
 
     perturbation(h, m, u, nc, cr);
 
-    e = malloc(n * sizeof *e);
-    eigenvalues(n, h, e);
+    e = malloc(nel * sizeof *e);
+    eigenvalues(nel, h, e);
 
-    mu = fermi_level(n, nel, e, m.kt, mu);
+    mu = fermi_level(nel, n, e, m.kt, mu);
 
-    energy = 2.0 * free_energy(n, nel, e, m.kt, mu);
+    energy = 2.0 * free_energy(nel, n, e, m.kt, mu);
 
-    for (i = 0; i < nx; i++)
-        for (j = 0; j < nx; j++)
+    for (i = 0; i < nph; i++)
+        for (j = 0; j < nph; j++)
             energy += 0.5 * u[i] * c[i][j] * u[j];
 
     printf("%.9f\n", energy);
 
-    occ = malloc(n * sizeof *occ);
-    occupations(n, occ, e, m.kt, mu);
+    occ = malloc(nel * sizeof *occ);
+    occupations(nel, occ, e, m.kt, mu);
 
-    forces = malloc(nx * sizeof *forces);
+    forces = malloc(nph * sizeof *forces);
     jacobian(h, m, occ, forces, nc, cr);
 
-    for (i = 0; i < nx; i++)
-        for (j = 0; j < nx; j++)
+    for (i = 0; i < nph; i++)
+        for (j = 0; j < nph; j++)
             forces[i] += c[i][j] * u[j];
 
-    for (i = 0; i < nx; i++)
+    for (i = 0; i < nph; i++)
         printf("% .9f\n", forces[i]);
 
     return 0;
