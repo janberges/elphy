c06653d4403578883371d14e2abaab153c23aef9
Jan Berges <jan.berges@uni-bremen.de>
2026-02-05 08:31:59 +0100

    Prefer single over double quotation marks

--- show.py
+++ show.py
@@ -2,4 +2,4 @@ import elphmod
 
 driver = elphmod.md.Driver.load('driver.pickle')
 driver.plot(interactive=True, scale=10, pause=0.1)
-driver.from_xyz("ipi.pos_0.xyz")
+driver.from_xyz('ipi.pos_0.xyz')
