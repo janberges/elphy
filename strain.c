dfc69b11cb7c1cb45af5b2dedeb1df06c98f445a
Jan Berges <jan.berges@uni-bremen.de>
2026-03-31 11:45:52 +0200

    Move negative sign from displacement to prefactor

--- strain.c
+++ strain.c
@@ -14,16 +14,16 @@ double strain_energy(const struct model m) {
         x = div(k->a, 3);
         y = div(k->b, 3);
 
-        ux = m.tau[x.quot][x.rem] - m.tau[y.quot][x.rem];
+        ux = m.tau[y.quot][x.rem] - m.tau[x.quot][x.rem];
         uy = m.tau[y.quot][y.rem] - m.tau[x.quot][y.rem];
 
         for (i = 0; i < 3; i++) {
-            ux -= m.r[k->r][i] * m.uc[i][x.rem];
+            ux += m.r[k->r][i] * m.uc[i][x.rem];
             uy += m.r[k->r][i] * m.uc[i][y.rem];
         }
 
         energy += ux * k->c * uy;
     }
 
-    return 0.25 * m.strain * m.strain * energy;
+    return -0.25 * m.strain * m.strain * energy;
 }
